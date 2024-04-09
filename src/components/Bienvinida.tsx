426954bc50e9f01d2383d425eaace76f949ebd34
Juan Cruz <gimenez.jc31@gmail.com>
2024-04-09 00:05:05 -0300

    actualizacion responsiveness

--- src/components/Bienvinida.tsx
+++ src/components/Bienvinida.tsx
@@ -1,22 +1,21 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Bienvenida() {
     return (
-        <div className="container mx-auto flex flex-col relative">
-            <div className="flex justify-center items-center mt-5 gap-5">
-                <h1 className="text-4xl font-bold text-white">¡Bienvenido a Brainstorm Lab!</h1>
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex flex-col justify-center items-center ">
+            <div className="flex flex-col justify-center items-center mt-5 gap-5">
+                <h1 className="text-4xl md:text-6xl font-bold text-white">¡Bienvenido a Brainstorm Lab!</h1>
                 <img
                     src="https://img.icons8.com/?size=100&amp;id=2885&amp;format=png&amp;color=ffffff"
                     alt="Microscope"
                     loading="lazy"
-                    className="w-12 h-12 text-white"
+                    className="w-12 h-12 md:w-20 md:h-20 text-white"
                 />
             </div>
-            <div className="flex justify-center items-center mt-20">
-                <h2 className="text-2xl font-bold text-white italic">¿Tienes una idea brillante? ¡Compártela con nosotros y hagámosla realidad juntos!</h2>
+            <div className="flex flex-col justify-center items-center mt-10 md:mt-20">
+                <h2 className="text-lg md:text-2xl font-bold text-white italic text-center">¿Tienes una idea brillante? ¡Compártela con nosotros y hagámosla realidad juntos!</h2>
             </div>
         </div >
     );
--- src/components/PhoneButton.tsx
+++ src/components/PhoneButton.tsx
@@ -12,8 +12,8 @@ export default function PhoneButton({ children, className, onClick, disabled = f
             disabled={true}
             className="cursor-not-allowed group flex justify-center items-center gap-2 group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-neutral-900 duration-500 hover:duration-500 underline underline-offset-2 hover:underline hover:underline-offset-4 origin-left hover:decoration-2 hover:text-neutral-300 relative bg-neutral-900 px-10 py-4 border text-left p-3 text-gray-50 text-base font-bold rounded-lg overflow-hidden after:absolute after:z-10 after:w-12 after:h-12 after:content[''] after:-left-8 after:top-8 after:rounded-full after:blur-lg hover:after:animate-pulse"
         >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
             </svg>
 
             Crear
